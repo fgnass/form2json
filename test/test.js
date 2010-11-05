1f65020fc972d5c74e32f219ccf468dcfb307f17
Felix Gnass <felix.gnass@neteye.de>
2010-11-05 19:43:05 +0100

    Added tests for the transform() function

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var form2json = require('form2json');
 
-function test(str, json) {
+function test(input, json) {
 	return function(assert) {
-		assert.eql(form2json.decode(str), json);
+		var obj = typeof input == 'string' ? form2json.decode(input) : form2json.transfomr(input);
+		assert.eql(obj, json);
 	};
 }
 
@@ -10,11 +11,13 @@ module.exports = {
 	strings: test('a=a&b=b', {a: 'a', b: 'b'}),
 	numbers: test('a=1&b=2.2', {a: 1, b: 2.2}),
 	nested: test('a=a&b.a=ba&b.b=bb', {a: 'a', b: {a: 'ba', b: 'bb'}}),
+	transformNested: test({a: 'a', 'b.a': 'ba', 'b.b': 'bb'}, {a: 'a', b: {a: 'ba', b: 'bb'}}),
 	deeplyNested: test('a.a.a=aaa&a.b=ab', {a: {a: {a: 'aaa'}, b: 'ab'}}),
+	transformDeeplyNested: test({'a.a.a' : 'aaa', 'a.b' : 'ab'}, {a: {a: {a: 'aaa'}, b: 'ab'}}),
 	array: test('a[0]=0&a[1]=1&a[2]=2', {a: [0, 1, 2]}),
 	reverseArray: test('a[2]=0&a[1]=1&a[0]=2', {a: [2, 1, 0]}),
 	orderedArray: test('a[x]=0&a[z]=1&a[y]=2', {a: [0, 1, 2]}),
 	arrayPush: test('a[]=0&a[]=1&a[]=2', {a: [0, 1, 2]}), 
 	nestedArrays: test('a[0][0]=a00&a[0][1]=a01&a[1][0]=a10&a[1][1]=a11', {a: [['a00', 'a01'], ['a10', 'a11']]}),
-	unescapedEqualSigns: test('a=b=b&b=c=c', {a: 'b=b', b: 'c=c'}),
+	unescapedEqualSigns: test('a=b=b&b=c=c', {a: 'b=b', b: 'c=c'})
 };
